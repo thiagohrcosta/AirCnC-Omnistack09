3c7d6e15b5fe0cbe64a556bdd7d7d819ed7e3307
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-20 23:59:12 -0300

    OmniStack Semana 09 - Instalando o multed

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,5 +1,10 @@
+const Spot = require('../models/Spot');
+
 module.exports = {
     async StorageEvent(req, res){
-        return res.json({ok : true})
+        const { filename } = req.file;
+        const { company, techs, price } = req.body;
+
+        return res.json({ ok : true })
     }
-}
+};

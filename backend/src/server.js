e50b8ede933b317cae36291d5c703b5632512dc7
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-20 22:23:51 -0300

    OmniStack Semana 09 - Entendendo o uso de GET/POST/PUT/DELETE e req

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json( {message: "Hello P"});
+// =============================================================
+// GET: método utilizado para buscar informação do backend
+// POST: quando se quer criar nova informação no backend
+// PUT : Informar que quer editar alguma informação
+// DELETE: deletar informação
+// =============================================================
+// req.query = Acessar query params (para filtros )
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+// =============================================================
 
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+    return res.json( {idade : req.query.idade});
 });
 
 app.listen(3333);

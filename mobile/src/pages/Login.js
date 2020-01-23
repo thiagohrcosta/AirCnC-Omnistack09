eb056f3148b1874c6eb110735f166cd1ecf4049a
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-23 13:41:34 -0300

    OmniStack Semana 09: Continuando a aplicar estilos no Login app

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Image, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 import logo from '../assets/logo.png';
 
@@ -19,7 +19,7 @@ export default function Login(){
                 autoCorrect={false}
             />
 
-            <Text style={styles.label}>Tecnologias *</Text>
+            <Text style={styles.label}>TECNOLOGIAS *</Text>
             <TextInput 
                 style={styles.input}
                 placeholder="Tecnologias de interesse"
@@ -28,6 +28,9 @@ export default function Login(){
                 autoCorrect={false}
             />
 
+            <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Encontrar spots</Text>
+                </TouchableOpacity>
         </View>
     </View> 
     );   
@@ -63,7 +66,22 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         borderRadius: 2
     },
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+
 });
+
+
 
 
 

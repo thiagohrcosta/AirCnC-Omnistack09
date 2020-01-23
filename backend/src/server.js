0dfbe7ae065e28a69964c0b0bd9495bb5ea64172
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-22 21:08:30 -0300

    OmniStack Semana 09: adicionando códigos ao backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -24,6 +25,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omni09-z0kfu.mongodb.net/sem
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);

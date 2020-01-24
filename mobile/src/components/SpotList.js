31491606c06c4d98cce0ee76a7d68c30bad5870c
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-24 00:12:23 -0300

    OmniStack Semana 09: Corrigindo bugs no App

--- mobile/src/components/SpotList.js
+++ mobile/src/components/SpotList.js
@@ -1,46 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import { withNavigation } from 'react-navigation';
 import { View, StyleSheet, Text, FlatList, Image, TouchableOpacity } from 'react-native';
 
 import api from '../services/api';
 
-export default function SpotList({ tech }){
+function SpotList({ tech }) {
     const [spots, setSpots] = useState([]);
 
     useEffect(() => {
-        async function loadSpots(){
-            const response = await api.get('./spots', {
+        async function loadSpots() {
+            const response = await api.get('/spots', {
                 params: { tech }
             })
 
             setSpots(response.data);
         }
 
+        loadSpots();
+
     }, []);
+
+    function handleNavigate(id) {
+        navigation.navigate('Book', { id })
+    }
 
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Empresas que usam <Text style={styles.bold}>{tech}</Text></Text>
 
-            <FlatList 
-                style={styles.list}
-                data={spots}
-                keyExtractor={spot => spot._id}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                renderItem={( { item } ) => (
-                    <View style={styles.listItem}>
-                        <Image 
-                        style={styles.thumbnail} 
-                        source={{uri: item.thumbnail_url}} />
-                        <Text style={styles.company}>{item.company}</Text>
-                        <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : 'GRATUITO'}</Text>
-                        <TouchableOpacity onPress={() => {}} style={styles.button}>
-                            <Text style={styles.buttonText}>Solicitar reserva</Text>
-                        </TouchableOpacity>
-                    </View>
-                ) }
-            />
-
+            <FlatList style={styles.list} data={spots} keyExtractor={spot => spot._id} horizontal showsHorizontalScrollIndicator={false} renderItem={({ item }) => (
+                <View style={styles.listItem}>
+                    <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }} />
+                    <Text style={styles.company}>{item.company}</Text>
+                    <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : 'GRATUITO'}</Text>
+                    <TouchableOpacity onPress={() => handleNavigate(item._id)} style={styles.button}>
+                        <Text style={styles.buttonText}>Solicitar reserva</Text>
+                    </TouchableOpacity>
+                </View>
+            )} />
         </View>
 
     )
@@ -105,3 +102,5 @@ const styles = StyleSheet.create({
         fontSize: 15,
     }
 });
+
+export default  withNavigation(SpotList);
